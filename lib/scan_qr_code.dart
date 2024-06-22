bf001c81994cb7f3ba880b6674b23cf42ad04969
BanamaliNayak <nayakbanamali00@gmail.com>
2024-06-22 17:17:21 +0530

    QR code scanner and generator
    Scan and generate code

---
+++ lib/scan_qr_code.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+
+class ScanQrCode extends StatefulWidget {
+  const ScanQrCode({super.key});
+
+  @override
+  State<ScanQrCode> createState() => _ScanQrCodeState();
+}
+
+class _ScanQrCodeState extends State<ScanQrCode> {
+
+  String qrResult = 'Scanned Data will appear here';
+  Future<void> scanQR()async{
+    try{
+      final qRCode = await FlutterBarcodeScanner.scanBarcode('#ff6666', 'Cancel', true, ScanMode.QR);
+      if(!mounted)return;
+      setState(() {
+        this.qrResult = qRCode.toString();
+      });
+    }on PlatformException{
+      qrResult = 'Fail to read QR Code';
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('QR Code Scanner',style: TextStyle(fontWeight: FontWeight.bold),),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(height: 30,),
+            Text('$qrResult',style: TextStyle(color: Colors.black,fontSize: 20)),
+            SizedBox(height: 30),
+            Container(
+              child: Center(
+                child: Icon(
+                  Icons.qr_code_scanner_outlined,
+                  color: Colors.purple,
+                  size: 100,
+                ),
+              ),
+            ),
+            SizedBox(height: 20,),
+            ElevatedButton(onPressed: scanQR, child: Text('Scan Code',
+              style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 21,
+              color: Colors.purple
+              ),
+            ),
+
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
